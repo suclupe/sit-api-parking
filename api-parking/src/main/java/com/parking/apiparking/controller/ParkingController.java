df230711a99a127ee7eeb4678f8b2747ba1e5fb0
Suclupe <jsuclupev1@upao.edu.pe>
2023-04-18 00:07:41 -0500

    feat: Crear la entidad Car

--- api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
+++ api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
@@ -1,4 +1,14 @@
 package com.parking.apiparking.controller;
 
+import java.time.LocalDateTime;
+
 public class ParkingController {
+    private String licensePlate;
+    private String color;
+    private LocalDateTime entryTime;
+
+    public ParkingController(String licensePlate, String color) {
+        this.licensePlate = licensePlate;
+        this.color = color;
+    }
 }
